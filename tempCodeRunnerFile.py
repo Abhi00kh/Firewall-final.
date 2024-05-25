297167a6c408088992a9263facc574769575a44c
1111 <102043779+Abhi00kh@users.noreply.github.com>
2024-05-25 14:27:18 +0530

    Delete tempCodeRunnerFile.py

--- tempCodeRunnerFile.py
+++
@@ -1,6 +0,0 @@
-from app import app, socketio
-from app import view  # Import view.py containing your Flask app code
-import json
-
-if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', debug=True, port=5001)
