f23c5b915e855b73ea85b6b101008d021f6ffd46
1111 <102043779+Abhi00kh@users.noreply.github.com>
2024-05-26 12:12:30 +0530

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,9 @@
+# from app import app, socketio
+# from app import view 
+# # Create a WSGI application for deployment
+# application = socketio.WSGIApp(app)
 from app import app, socketio
 from app import view 
+
 # Create a WSGI application for deployment
-application = socketio.WSGIApp(app)
+application = socketio.Middleware(app, socketio)
